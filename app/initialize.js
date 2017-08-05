d76b92b36d6523aa5d5eb7bd53ec3bfc705afcfc
Dave Mackie <dave.mackie@gmail.com>
2017-08-05 15:22:34 +0200

    Add Datamap showing legality based on data from google spreedsheet

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,55 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // do your setup here
-  console.log('Initialized app');
-});
+  console.log('Initialized app')
+})
+
+var d3 = require('d3')
+// var Topojson = require('topojson')
+var Datamaps = require('datamaps')
+var Tabletop = require('tabletop')
+
+var map = null
+
+const countryDataUrl = 'https://docs.google.com/spreadsheets/d/1PW_2A7786oLaSszO1wsHv-J59yn7ov7dgp9OOehSSNI/pubhtml'
+
+// var key = '1PW_2A7786oLaSszO1wsHv-J59yn7ov7dgp9OOehSSNI'
+// //the url for jQuery and D3
+// var url = "https://spreadsheets.google.com/feeds/list/" + key + "/od6/public/values?alt=json";
+
+const processCountryData = function (data, tabletop) {
+  var sheet1 = data['Sheet1']
+  console.log(data['Sheet1'])
+  var countryData = {}
+  sheet1.elements.forEach(function (o) {
+    countryData[o.id] = o
+  })
+  drawmap(countryData)
+}
+
+const drawmap = function (countryData) {
+  map = new Datamaps({
+    element: document.getElementById('container'),
+    responsive: true,
+    fills: {
+      'decriminalised': '#e41a1c',
+      'illegal': '#377eb8',
+      '#N/A': '#4daf4a',
+      'medical': '#984ea3',
+      'tolerated': '#ff7f00',
+      defaultFill: '#EDDC4E'
+    },
+    data: countryData,
+    geographyConfig: {
+      popupTemplate: function (geo, data) {
+        //  console.log({geo, data})
+        //  var value = data.fillKey// ? data.fillKey : 'Unknown'
+        return `<div class="hoverinfo"><strong> ${data.name} <br/> Legality: ${data.fillKey} </strong></div>`
+      }
+    }
+  })
+}
+
+Tabletop.init({
+  key: countryDataUrl,
+  callback: processCountryData
+})

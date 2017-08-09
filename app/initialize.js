48768c307602b3f94a94039bdb1670a3f54c8625
Dave Mackie <dave.mackie@gmail.com>
2017-08-09 15:24:21 +0200

    [WIP] adding zoom and pan to map *not working*

--- app/initialize.js
+++ app/initialize.js
@@ -7,8 +7,8 @@ var d3 = require('d3')
 var Topojson = require('topojson')
 var Datamaps = require('datamaps')
 var Tabletop = require('tabletop')
-var _ = require("lodash")
-var $ = require("jquery");
+var _ = require('lodash')
+var $ = require('jquery')
 
 var map = null
 
@@ -21,11 +21,11 @@ const countryDataUrl = 'https://docs.google.com/spreadsheets/d/1PW_2A7786oLaSszO
 const processCountryData = function (data, tabletop) {
   var sheet1 = data['Sheet1']
   console.log(data['Sheet1'])
-  var fills = {};
+  var fills = {}
   data.Keys.elements.forEach(key => {
-    fills[key.key] = key.colour;
-  });
-  var keys = data.Keys.elements;
+    fills[key.key] = key.colour
+  })
+  var keys = data.Keys.elements
   var countryData = {}
   sheet1.elements.forEach(function (o) {
     countryData[o.id] = o
@@ -44,16 +44,22 @@ const drawmap = function (countryData, fills) {
       popupTemplate: function (geo, data) {
         return `<div class="hoverinfo"><strong> ${data.name} <br/> Legality: ${data.keyname} </strong></div>`
       }
+    },
+    done: (datamap) => {
+      datamap.svg.call(d3.zoom().on('zoom', redraw))
+      function redraw () {
+        datamap.svg.attr('transform', d3.event.transform)
+      }
     }
   })
 }
 
 const drawKey = keys => {
-  let template = _.template($("#legendKeyTemplate").html());
+  let template = _.template($('#legendKeyTemplate').html())
   keys.forEach(key => {
-    $("#legend").append(template({key}));
+    $('#legend').append(template({key}))
   })
-};
+}
 
 Tabletop.init({
   key: countryDataUrl,

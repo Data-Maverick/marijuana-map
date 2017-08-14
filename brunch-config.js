d7dedf294074ef78f9fe604dbcb2d9feb8430ddb
Jason Norwood-Young <jason@10layer.com>
2017-08-14 15:13:47 +0200

    Minify

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,16 @@
 // See http://brunch.io for documentation.
 exports.files = {
   javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+  stylesheets: {joinTo: 'app.css'},
 };
+
+exports.plugins = {
+    uglify: {
+      mangle: false,
+      compress: {
+        global_defs: {
+          DEBUG: false
+        }
+      }
+    }
+  };
